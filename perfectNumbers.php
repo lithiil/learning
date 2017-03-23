3620a430d3cac45b3fda7cf3ed2df067f851e5b7
tudorpalade <tudorjamal@gmail.com>
2017-03-23 10:13:32 +0200

    3rd exercise added calculation time

--- perfectNumbers.php
+++ perfectNumbers.php
@@ -8,7 +8,11 @@
 
 $num = $argv[1];
 
+
+
 function perfectNumbers($num) {
+    $time_start = microtime(true);
+
     $divisors = [];
     $tmpNum = $num;
 
@@ -18,12 +22,17 @@ function perfectNumbers($num) {
         }
     }
 
+    $time_end = microtime(true);
+    $time = $time_end - $time_start;
+
     if(array_sum($divisors) == $num) {
-        echo "The number is perfect".PHP_EOL;
+        echo "The number is perfect "."Calculated in ".intval(round($time))." seconds".PHP_EOL;
     } else {
-        echo "The number is not perfect".PHP_EOL;
+        echo "The number is not perfect"."Calculated in ".intval(round($time))." seconds".PHP_EOL;
     }
 
 }
+
+
 
 perfectNumbers($num);
