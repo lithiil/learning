cdb84731bcd08a8de64e6b33c90d085d8f63fdcb
lithiil <tudrjamal@gmail.com>
2017-03-23 21:12:54 +0200

    isPrime added

---
+++ isPrime.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lithiil
+ * Date: 23.03.2017
+ * Time: 20:56
+ */
+
+$num = $argv[1];
+
+function isPrime($num)
+{
+
+    $tmpNum = $num;
+
+    for ($i = 2; $i <= 10; $i++) {
+
+
+    }
+}
